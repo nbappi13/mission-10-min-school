488ab3169344770bc8fd09f5cae6b9239b236f48
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-07-26 19:07:20 +0600

    start from fresh due some unavoidable issues: edit page.tsx, layout.tsx, globals.css and deleted some contents and a file named page.module.css

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cdn.10minuteschool.com'], // ✅ allows external images to be optimized by Next.js
+  },
 };
 
 export default nextConfig;
