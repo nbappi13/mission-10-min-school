ef98b9f8eee0f58fd5d5a9f75aead5881af908f2
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-07-27 23:59:28 +0600

    updated ProductBanner file

--- src/components/ProductBanner.tsx
+++ src/components/ProductBanner.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Image from "next/image"
 
 type ProductBannerProps = {
@@ -11,8 +9,15 @@ export default function ProductBanner({ imageUrl }: ProductBannerProps) {
   const bannerImage = imageUrl || "https://cdn.10minuteschool.com/images/dasktop_banner_1753270611489.png?w=1800"
 
   return (
-    <div className="relative w-full h-[200px] md:h-[300px]">
-      <Image src={bannerImage || "/placeholder.svg"} alt="IELTS Course Banner" fill className="object-cover" priority />
+    <div className="relative w-full">
+      <Image
+        src={bannerImage || "/placeholder.svg"}
+        alt="IELTS Course Banner"
+        width={1800}
+        height={400}
+        className="w-full h-auto object-contain"
+        priority
+      />
     </div>
   )
 }
